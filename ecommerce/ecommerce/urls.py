8a7f886c948c5aa82f5ef8f5339314c75b752c17
Leonard Rodriguez <leonardrodriguezr92@gmail.com>
2022-08-04 17:48:16 -0400

    database

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from ecommerce.ecommerce.views import products
-from products.views import familia
+from products.views import products
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', products, name='familia')
+    path('products/', products, name='products')
 ]
