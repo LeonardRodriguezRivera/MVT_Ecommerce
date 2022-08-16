8acb393c181306db8045c0863cbd35df1f8f47d7
Leonard Rodriguez <leonardrodriguezr92@gmail.com>
2022-08-15 20:48:32 -0400

    databasestock

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class Products(models.Model):
     name = models.CharField(max_length=40)
     price = models.FloatField(max_length=40)
-    Stock = models.IntegerField
     creation_date = models.DateField(auto_now_add=True, null=True, blank=True)
-    #email = models.EmailField()
-    
+    stock = models.IntegerField()
+    email = models.EmailField()
 
 class Distributor(models.Model):
     name = models.CharField(max_length=40)
