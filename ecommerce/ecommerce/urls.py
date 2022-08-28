379f7f2c81de29b8099acafd14a97d5f3782ff3a
leomatias <lmatias.mansilla@hotmail.com.ar>
2022-08-28 17:23:47 -0300

    Se agrega el error 404

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from products.views import products
-from ecommerce.views import products, index, about
+from ecommerce.views import products, index, about, error_404
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import handler404
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +16,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('users/', include('users.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = error_404.as_view()
